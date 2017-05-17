56cf76f0970f68362e0b57f710b7b0fb0c7745a9
imediasun <imediasun@imediasun.freelance-box.net>
2017-05-17 18:57:50 +0500

    json request_for_nechemya

--- app/Http/Middleware/ApiUserAuth.php
+++ app/Http/Middleware/ApiUserAuth.php
@@ -15,6 +15,41 @@ class ApiUserAuth
      */
     public function handle($request, Closure $next)
     {
+        $authToken = $request->bearerToken();
+
+        try {
+            $this->payloadIsValid(
+                $payload = (array) JWT::decode($authToken, 'w5yuCV2mQDVTGmn3', ['HS256'])
+            );
+
+            $app = Application::whereKey($payload['iss'])->firstOrFail();
+
+            $user = User::whereEmail($payload['sub'])->firstOrFail();
+        } catch (\Throwable $e) {
+            return response('token_invalid', 401);
+        }
+
+        if (! $app->is_active) {
+            return response('app_inactive', 403);
+        }
+
+        $request->merge(['__authenticatedApp' => $app]);
+
+        $request->merge(['__authenticatedUser' => $user]);
+
         return $next($request);
+    }
+
+    private function payloadIsValid($payload)
+    {
+        $validator = Validator::make($payload, [
+            'iss' => 'required',
+            'sub' => 'required',
+            'jti' => 'required',
+        ]);
+
+        if (! $validator->passes()) {
+            throw new \InvalidArgumentException;
+        }
     }
 }
